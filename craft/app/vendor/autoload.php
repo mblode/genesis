7dcf562c052019cc691046ac432cecda708fe22a
Matthew Blode <m.blode@gmail.com>
2017-07-25 23:13:55 +0900

    update

--- craft/app/vendor/autoload.php
+++ craft/app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb82aef0ba1f9155bc3a426d548e71c0b::getLoader();
+return ComposerAutoloaderInit044fd0b4692320a10a28f9e509303afb::getLoader();
